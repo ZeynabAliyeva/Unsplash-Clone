7a78d2c5959c630ac0a2c9e8697b8de0d10addeb
Zeynab <zeynab@MacBook-Air-Zeynab.local>
2023-03-08 14:56:40 +0400

    add profil page

--- src/features/ProfilFeatures/index.js
+++ src/features/ProfilFeatures/index.js
@@ -1,9 +1,30 @@
-import React from 'react'
-
+import React from "react";
+import profilImg from "../../assets/images/user.jpeg";
+import photosIcon from "../../assets/icons/photos.svg";
+import "./index.css";
 function ProfilFeatures() {
   return (
-    <div>ProfilFeatures</div>
-  )
+    <div className="profilContainer">
+      <div className="profileHeader">
+        <div className="profilInfo">
+          <div className="profilLeftBox">
+            <img src={profilImg} />
+          </div>
+          <div className="profilRightBox">
+            <h1>Anita Austvika
+              <button className="editPhotoBtn">Edit profile photo</button>
+            </h1>
+            <p>Download free, beautiful high-quality photos curated by Anita</p>
+          </div>
+        </div>
+        <div className="profilPhotosBox">
+          <img src={photosIcon} />
+          <p>Photos: 0</p>
+        </div>
+        <hr />
+      </div>
+    </div>
+  );
 }
 
-export default ProfilFeatures
+export default ProfilFeatures;
