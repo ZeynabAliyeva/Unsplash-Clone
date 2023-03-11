1812c33b591cd4f8465966784324872b9370254d
Zeynab <zeynab@MacBook-Air-Zeynab.local>
2023-03-12 02:30:40 +0400

    show post in homepage

--- src/features/HomeFeatures/index.js
+++ src/features/HomeFeatures/index.js
@@ -1,20 +1,28 @@
-import * as React from "react";
 import Box from "@mui/material/Box";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import "./index.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { api } from "../../network/api";
 
 function HomeFeatures() {
   const [hoveredIndex, setHoveredIndex] = useState(-1);
+  const [data, setData] = useState(null);
 
-  const handleMouseEnter = (index) => {
-    setHoveredIndex(index);
+  const handleMouseEnter = (id) => {
+    setHoveredIndex(id);
   };
 
   const handleMouseLeave = () => {
     setHoveredIndex(-1);
   };
+
+  useEffect(() => {
+    api.getAll("/posts").then((res) => {
+      setData(res);
+    });
+  }, []);
+
   return (
     <div className="imgBox">
       <Box sx={{ width: "90%", height: "100vh" }}>
@@ -24,23 +32,23 @@ function HomeFeatures() {
           gap={40}
           className="image-wrapper"
         >
-          {itemData.map((item, index) => (
+          {data?.map((item) => (
             <ImageListItem
-              key={item.img}
+              key={item._id}
               className="image-list-item"
-              onMouseEnter={() => handleMouseEnter(index)}
+              onMouseEnter={() => handleMouseEnter(item._id)}
               onMouseLeave={handleMouseLeave}
             >
               <img
                 className="imgs"
-                src={`${item.img}?w=248&fit=crop&auto=format`}
-                srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                alt={item.title}
+                src={`http://localhost:8080${item.imageUrl}?w=248&fit=crop&auto=format`}
+                srcSet={`http://localhost:8080${item.imageUrl}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                alt={item.text}
                 loading="lazy"
               />
-              {hoveredIndex === index && (
+              {hoveredIndex === item._id && (
                 <div className="image-overlay">
-                  <p className="image-title">{item.title}</p>
+                  <p className="image-title">{item.text}</p>
                   <button className="hoverDeleteBtn">Delete</button>
                 </div>
               )}
@@ -52,54 +60,4 @@ function HomeFeatures() {
   );
 }
 
-const itemData = [
-  {
-    img: "https://images.unsplash.com/photo-1549388604-817d15aa0110",
-    title: "Bed",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1525097487452-6278ff080c31",
-    title: "Books",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1523413651479-597eb2da0ad6",
-    title: "Sink",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1563298723-dcfebaa392e3",
-    title: "Kitchen",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1588436706487-9d55d73a39e3",
-    title: "Blinds",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1574180045827-681f8a1a9622",
-    title: "Chairs",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1530731141654-5993c3016c77",
-    title: "Laptop",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1481277542470-605612bd2d61",
-    title: "Doors",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1517487881594-2787fef5ebf7",
-    title: "Coffee",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1516455207990-7a41ce80f7ee",
-    title: "Storage",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62",
-    title: "Candle",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1519710164239-da123dc03ef4",
-    title: "Coffee table",
-  },
-];
 export default HomeFeatures;
